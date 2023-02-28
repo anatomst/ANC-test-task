d8ddeb6420dbe20e527258bbda0fd1897046e5e9
anatomst <www.shpachyk98@gmail.com>
2023-03-01 00:39:33 +0200

    edited Link model, added staticmethod generate_short_url

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -2,25 +2,29 @@ import random
 import string
 
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Link(models.Model):
     long_url = models.URLField()
     short_url = models.CharField(max_length=6, unique=True)
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    ip_address = models.GenericIPAddressField(null=True)
+    creator = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now=True)
     clicks = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = self.generate_short_url()
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_short_url():
+        while True:
+            short_url = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+            if not Link.objects.filter(short_url=short_url).exists():
+                return f"http://127.0.0.1:8000/redirect/{short_url}"
 
     def __str__(self):
         return self.short_url
-
-    # def save(self, *args, **kwargs):
-    #     request = kwargs.pop('request', None)
-    #     if not self.pk:
-    #         if request is not None and not request.user.is_authenticated:
-    #             self.ip_address = request.META.get('REMOTE_ADDR')
-    #     super().save(*args, **kwargs)
-# Link.objects.create(long_url="http://127.0.0.1:8000/iaudgcshbiaus")

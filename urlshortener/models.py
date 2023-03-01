51c1cee0360c2cd90c291279081f2906a956f9cd
anatomst <www.shpachyk98@gmail.com>
2023-03-01 21:59:34 +0200

    test version of Link model

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -4,27 +4,32 @@ import string
 from django.db import models
 
 
+def generate_short_symbol():
+    while True:
+        symbol = ''.join(random.choice(string.ascii_letters + string.digits + "-") for _ in range(6))
+        if not Link.objects.filter(short_symbol=symbol).exists():
+            return symbol
+
+
 class Link(models.Model):
     long_url = models.URLField()
-    short_url = models.CharField(max_length=6, unique=True)
+    short_symbol = models.CharField(max_length=6, unique=True, default=generate_short_symbol)
     creator = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
     ip_address = models.GenericIPAddressField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted = models.BooleanField(default=False)
-    timestamp = models.DateTimeField(auto_now=True)
-    clicks = models.PositiveIntegerField(default=0)
 
-    def save(self, *args, **kwargs):
-        if not self.short_url:
-            self.short_url = self.generate_short_url()
-        super().save(*args, **kwargs)
-
-    @staticmethod
-    def generate_short_url():
-        while True:
-            short_url = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-            if not Link.objects.filter(short_url=short_url).exists():
-                return f"http://127.0.0.1:8000/redirect/{short_url}"
+    def short_url(self):
+        return f"http://127.0.0.1:8000/redirect/{self.short_symbol}"
 
     def __str__(self):
         return self.short_url
+
+
+class Click(models.Model):
+    link = models.OneToOneField(Link, on_delete=models.CASCADE, related_name="clicks")
+    last_clicked_date = models.DateTimeField(auto_now=True)
+    clicks_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.link.short_url}: {self.clicks_count}"

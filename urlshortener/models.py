059c97b7992c6682c594b8474098c762b9296bb6
anatomst <www.shpachyk98@gmail.com>
2023-03-02 01:39:24 +0200

    refactoring

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -3,24 +3,31 @@ import string
 
 from django.db import models
 
+from ANC_test_task.settings import SHORT_HOST
 
-def generate_short_symbol():
+
+def generate_short_url():
+    """
+    Function to generate random symbol of 6 characters
+    :return: example: "6NpxdR"
+    """
+
     while True:
-        symbol = ''.join(random.choice(string.ascii_letters + string.digits + "-") for _ in range(6))
-        if not Link.objects.filter(short_symbol=symbol).exists():
-            return symbol
+        short_url = ''.join(random.choice(string.ascii_letters + string.digits + "-") for _ in range(6))
+        if not Link.objects.filter(short_url=short_url).exists():
+            return SHORT_HOST + short_url
 
 
 class Link(models.Model):
     long_url = models.URLField()
-    short_symbol = models.CharField(max_length=6, unique=True, default=generate_short_symbol)
+    short_url = models.CharField(max_length=255, unique=True, default=generate_short_url)
     creator = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
     ip_address = models.GenericIPAddressField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    deleted = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
 
-    def short_url(self):
-        return f"http://127.0.0.1:8000/redirect/{self.short_symbol}"
+    def get_symbol(self):
+        return self.short_url.split("/")[-1]
 
     def __str__(self):
         return self.short_url

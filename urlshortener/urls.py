92d1a7351fd7d5949b849dad87b2ce5b91fe6e8a
anatomst <www.shpachyk98@gmail.com>
2023-03-02 16:13:41 +0200

    added create_link_using_api

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from urlshortener.views import LinkCreateViewAPI, LinkCreateView, LinkStatsView, LinkRedirectView, LinkDeleteView, \
-    LinkDeleteConfirmView
+    LinkDeleteConfirmView, LinkCreateViewUsingAPI
 
 urlpatterns = [
     path("api/shortner/", LinkCreateViewAPI.as_view(), name="create_short_url_api"),
+    path('create-link/', LinkCreateViewUsingAPI.as_view(), name='create_link_using_api'),
+
     path('shortner/', LinkCreateView.as_view(), name='shortner'),
     path('shortner/<str:symbol>/stats/', LinkStatsView.as_view(), name='link_detail'),
     path('shortner/<str:symbol>/confirm/', LinkDeleteConfirmView.as_view(), name='link_confirm'),
